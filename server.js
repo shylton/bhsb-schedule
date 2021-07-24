e5b167b3e3fd7c2d4b66920d249231b6d7141076
shylton <mattas@oregonstate.edu>
2021-07-24 19:06:52 -0400

    ready for heroku deploy 2

--- server.js
+++ server.js
@@ -9,12 +9,23 @@ const PORT = process.env.PORT || 5000
 
 connectDB()
 
-app.get('/', (req, res) => {
-    res.json({ msg: 'Welcome to the bhsb schedule!' })
-})
-
 app.use('/api/events', require('./routes/events'))
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static(path.join(__dirname, 'client', 'build')))
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    )
+} else {
+    app.get('/', (req, res) => {
+        res.json({ msg: 'Welcome to the bhsb schedule!' })
+    })
+}
+
+// fallback if all else fails
 app.use(function (req, res, next) {
     console.log('@server.js: 404 error')
     res.status(404).send("404 ERROR. Sorry can't find that!")
